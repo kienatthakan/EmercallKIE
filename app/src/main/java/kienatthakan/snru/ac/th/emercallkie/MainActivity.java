c93118e43abb905924cd0194fcf90b89fadb35e3
kienatthakan <kienatthakan@gmail.com>
2018-02-19 14:36:43 +0700

    Initial commit

---
+++ app/src/main/java/kienatthakan/snru/ac/th/emercallkie/MainActivity.java
@@ -0,0 +1,13 @@
+package kienatthakan.snru.ac.th.emercallkie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
