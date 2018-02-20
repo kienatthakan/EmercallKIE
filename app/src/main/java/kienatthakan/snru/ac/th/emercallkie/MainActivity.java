330fa1082b96bd44658055fb8fdd7027b78fa5ce
kienatthakan <kienatthakan@gmail.com>
2018-02-20 12:01:10 +0700

    Add Fragment Success

--- app/src/main/java/kienatthakan/snru/ac/th/emercallkie/MainActivity.java
+++ app/src/main/java/kienatthakan/snru/ac/th/emercallkie/MainActivity.java
@@ -1,7 +1,10 @@
 package kienatthakan.snru.ac.th.emercallkie;
 
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kienatthakan.snru.ac.th.emercallkie.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+ //       Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+
+
+    } // Main Method
+} // Main Class
