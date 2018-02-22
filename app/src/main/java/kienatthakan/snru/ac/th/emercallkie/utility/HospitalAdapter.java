5b181d8ad767a76399c0499274955cc8265ef4fa
kienatthakan <kienatthakan@gmail.com>
2018-02-22 10:45:16 +0700

    1/22feb

---
+++ app/src/main/java/kienatthakan/snru/ac/th/emercallkie/utility/HospitalAdapter.java
@@ -0,0 +1,52 @@
+package kienatthakan.snru.ac.th.emercallkie.utility;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+/**
+ * Created by Admin on 22/2/2561.
+ */
+
+public class HospitalAdapter extends BaseAdapter{
+
+//    Explicit
+    private Context context;
+    private int[] ints;
+    private String[] titleStrings,phoneStrings;
+
+    public HospitalAdapter(Context context, int[] ints, String[] titleStrings, String[] phoneStrings) {
+        this.context = context;
+        this.ints = ints;
+        this.titleStrings = titleStrings;
+        this.phoneStrings = phoneStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return ints.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+
+        return null;
+    }
+}  //Main Class
