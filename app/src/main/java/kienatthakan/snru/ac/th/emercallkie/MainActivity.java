6d17f18b2038ca39fa960dbc8bdc42cb3a5271a3
kienatthakan <kienatthakan@gmail.com>
2018-02-22 16:36:57 +0700

    Update 22feb

--- app/src/main/java/kienatthakan/snru/ac/th/emercallkie/MainActivity.java
+++ app/src/main/java/kienatthakan/snru/ac/th/emercallkie/MainActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import kienatthakan.snru.ac.th.emercallkie.fragment.HospifalFragment;
 import kienatthakan.snru.ac.th.emercallkie.fragment.MainFragment;
 
 
@@ -22,6 +25,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Police Controller
+        TextView policeTextView = findViewById(R.id.txtPolice);
+        policeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment,new MainFragment()).commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+//        Hospital Controller
+        TextView hospitalTextView = findViewById(R.id.txtHospital);
+        hospitalTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment,new HospifalFragment()).commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+//        Insurance Controller
+        TextView insuranceTextView = findViewById(R.id.txtInsurance);
+        insuranceTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment,new MainFragment()).commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
+//        Exit Controller
+        TextView ExitTextView = findViewById(R.id.txtExit);
+        ExitTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment,new MainFragment()).commit();
+                drawerLayout.closeDrawers();
+
+            }
+        });
+
 
 //        Create Toolbar
         Toolbar toolbar = findViewById(R.id.toolbarMain);
--- app/src/main/java/kienatthakan/snru/ac/th/emercallkie/fragment/HospifalFragment.java
+++ app/src/main/java/kienatthakan/snru/ac/th/emercallkie/fragment/HospifalFragment.java
@@ -1,14 +1,17 @@
 package kienatthakan.snru.ac.th.emercallkie.fragment;
 
 
-import android.app.Fragment;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import kienatthakan.snru.ac.th.emercallkie.R;
+import kienatthakan.snru.ac.th.emercallkie.utility.HospitalAdapter;
 
 /**
  * Created by Admin on 22/2/2561.
@@ -19,7 +22,27 @@ public class HospifalFragment extends Fragment{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-    }
+
+//        Create ListView
+        ListView listView = getView().findViewById(R.id.lisViewHopotal);
+
+        int[]ints =new int[]{R.drawable.station1,R.drawable.station2,
+
+                R.drawable.station3,R.drawable.station4, R.drawable.station1,
+                R.drawable.station2,R.drawable.station3,R.drawable.station4};
+
+        String[]titleStrings = new String[]{"Hospital 1","Hospital 2","Hospital 3","Hospital 4",
+                "Hospital 5","Hospital 6","Hospital 7","Hospital 8",};
+
+        String[] phoneString = new String []{"1111", "1112", "1113", "1114", "1115", "1116",
+                "1117", "1118"};
+
+        HospitalAdapter hospitalAdapter = new HospitalAdapter(getActivity(), ints, titleStrings, phoneString);
+        listView.setAdapter(hospitalAdapter);
+
+
+
+    }   //    Main Method
 
     @Nullable
     @Override
